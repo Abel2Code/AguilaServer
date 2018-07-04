ed201c2a151fffbc566c3fe5a2de7b60b3028997
Abel2Code <asalinaswc1@aol.com>
2018-07-04 12:20:42 -0700

    Implemented Messages

--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Messages = require('../models/messages');
+const JobBoard = require('../models/jobBoard')
 
 router.get('/job', function(req, res) {
   Messages.find({
@@ -16,6 +17,27 @@ router.get('/job', function(req, res) {
       res.send(messages);
     }
   });
+})
+
+router.put('/sendMessage', function(req, res) {
+  JobBoard.findOne({_id: req.body.job}, function(err, jobBoard){
+    if(err){
+      res.send(err);
+      return;
+    }
+    let isMentor = true;
+    if(req.body.userId === jobBoard.author){
+      isMentor = false;
+    }
+    jobBoard.messages.push({
+      isMentor: isMentor,
+      message: req.body.message
+    })
+
+    jobBoard.save(function(err){
+       if (err) return handleError(err);
+    })
+  })
 })
 
 module.exports = router;
