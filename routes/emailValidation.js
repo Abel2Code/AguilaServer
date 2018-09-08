d26340e1d6cecfa56a9d40687c980b8ddb464f3d
Abel2Code <asalinaswc1@aol.com>
2018-09-07 23:44:12 -0700

    Fixed Signup bug

--- routes/emailValidation.js
+++ routes/emailValidation.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 
-const AuthPin = require('../models/AuthPin');
+const AuthPin = require('../models/authPin');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
